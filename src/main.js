73677f21a012e2881f688bebff5a28e8602400d7
Mariem <tlichmariem@gmail.com>
2024-10-16 10:36:57 +0200

    add

--- src/main.js
+++ src/main.js
@@ -2,7 +2,10 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import store from './store';
 import router from './router';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+
 
 
 const app = createApp(App);
