516f36ea336e1fef81047c7c62ca885f86e0af19
Axel Le Berre <axel.le.berre@gmail.com>
2024-10-16 13:09:52 +0200

    finition nav bar et footer

--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -1,56 +1,6 @@
 <template>
-
-<div class="text-end me-5 mb-4">
-      <div class="d-flex flex-column align-items-center">
-          <a href="/login"> <img src="../assets/user.png" alt="User Icon" class="icon"> </a>
-          <a href="/login" class="btn btn-link">Connexion</a>
-      </div>
-      <div class="d-flex flex-column align-items-center">
-         <a href="/signup"> <img src="../assets/signup.png" alt="Sign Up Icon" class="icon"> </a>
-          <a href="/signup" class="btn btn-link">Inscription</a>
-      </div>
-  </div>  
-
-  <!-- Code navbar -->
-    <nav class="navbar">
-      <div class="navbar-logo">
-        <router-link to="">
-          <img src="../assets/logo.png" alt="logo" class="logo ms-4">
-        </router-link>
-      </div>
-      <div class="navbar-container">
-        <!-- Logo -->
-  
-        <!-- Navigation Links -->
-        <ul class="navbar-links">
-          <li><router-link to="/">Accueil</router-link></li>
-          
-          <!-- Dropdown Menu -->
-          <li class="dropdown" @mouseenter="showDropdown = true" @mouseleave="showDropdown = false">
-            <span><router-link to="/ProductsListPage">Produits</router-link></span>
-            <ul v-if="showDropdown" class="dropdown-menu">
-              <li><router-link to="">Catégorie 1</router-link></li>
-              <li><router-link to="">Catégorie 2</router-link></li>
-              <li><router-link to="">Catégorie 3</router-link></li>
-              <li><router-link to="">Catégorie 4</router-link></li>
-            </ul>
-          </li>
-          <li><router-link to="/ProductDetails">Details de Produits</router-link></li>
-          <li><router-link to="/cart">Panier</router-link></li>
-          <li><router-link to="/checkout">Caisse</router-link></li>
-          <li><router-link to="/admin">Admin</router-link></li>
-        </ul>
-      </div>
-      <form class="d-flex me-4" role="search">
-              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-              <button class="btn btn-info me-2" type="submit">Search</button>
-          </form>
-    </nav> 
-    <!-- Fin navbar -->
-  
-    
-     <!-- Header -->
-      <div class="row">
+   <!-- Header -->
+   <div class="row">
           <div class="col-12 text-center pt-3">
               <h1>Web Wares</h1>
               <p>Bienvenue sur WebWares, votre 
@@ -76,7 +26,7 @@
       </div>
   </div>
 <!-- Fin header -->
-<br><br><br>
+  <br>
 <!-- bande pub cadeau reduction -->
 <div class="banner">
   <a href="#" class="banner-link">
@@ -168,103 +118,33 @@
         </form>
     </div>
 
-<!-- Footer -->
-<footer class="footer">
-    <div class="container">
 
-
-      <!-- Social Media, Payment Methods, and Delivery Methods Section -->
-      <div class="footer-links big-footer-bottom">
-        
-        <div class="column footer-top ">
-          <h5>Moyens de paiement</h5>
-          <ul>
-            <li> <img src="../assets/paiement/visa.png" alt="Visa" class="icon"> </li>
-            <li> <img src="../assets/paiement/applepay.png" alt="Apple Pay" class="icon ms-2 me-2"> </li>
-            <li> <img src="../assets/paiement/mastercard.png" alt="MasterCard" class="icon me-2"> </li>
-            <li> <img src="../assets/paiement/paypal.png" alt="PayPal" class="icon"> </li>
-          </ul>
-        </div>
-
-        <div class="column footer-top ms-5 me-5">
-          <h5>Suivez-nous sur les réseaux !</h5>
-          <ul>
-            <li>
-              <a href="#"> <img src="../assets/facebook.png" alt="logo facebook" class="logo"> </a>
-            </li>
-            <li>
-              <a href="#"> <img src="../assets/twitter.png" alt="logo twitter" class="logo ms-2 me-2"> </a>
-            </li>
-            <li>
-              <a href="#"> <img src="../assets/linkedin.png" alt="logo linkedin" class="logo"> </a>
-            </li>
-          </ul>
-        </div>
-        
-        <div class="column footer-top">
-          <h5>Moyens de livraison</h5>
-          <ul>
-            <li> <img src="../assets/dhl.png" alt="DHL" class="small"> </li>
-            <li> <img src="../assets/ups.png" alt="UPS" class="logo ms-2 me-2"> </li>
-            <li> <img src="../assets/fedex.png" alt="fedex" class="small"> </li>
-          </ul>
-        </div>
-      </div>
-
-      <!-- truspilot rating -->
-
-<div class="banner-row">
-        <!-- Trustpilot Column -->
-        <div class="trustpilot-column">
-            <img src="../assets/trustpilot.png" alt="Trustpilot" width="130" height="90" class="pb-3 pe-2">
-            <p><span>Nos clients adorent Web Wares et nous le disent :</span><br>
-              <strong>4,1 étoiles sur 5</strong> (8,372 avis clients)</p>
-        </div>
-        <!-- App Store Column -->
-        <div class="app-column">
-            <a href="#"><img src="../assets/app-store.png" alt="App Store" width="40"></a>
-            <a href="#"><img src="../assets/app.png" alt="Google Play" width="40"></a>
-            <p>Téléchargez l'appli Web Wares</p>
-        </div>
-        
-    </div>
-    <br>
-
-<!-- fin truspilot -->
-
-      <!-- Legal Links Section -->
-      <div>
-        <h5>Liens légaux</h5>
-        <ul class="legal-links">
-          <li><a href="#">Mentions légales</a></li>
-          <li><a href="#">Politique de confidentialité</a></li>
-          <li><a href="#">Conditions d'utilisation</a></li>
-        </ul>
-        <p class="footer-bottom">&copy; 2024 Mon Entreprise. Tous droits réservés.</p>
-      </div>
-    </div>
-  </footer>
-
-  <!-- Fin footer -->
 </template>
 
 
-<script>
-export default {
-  methods: {
-    subscribe() {
-      alert('Inscription à la newsletter réussie !');
-    }
-  },
-  data() {
-      return {
-        showDropdown: false,
-      };
-    },
-}
-</script>
 
 <style scoped>
+
+/* style pour header */
+.overflow {
+    position: relative;
+      overflow: hidden;
+  }
+  
+  .zoom img {
+      transition: all 0.2s linear;
+    }
+  
+  .zoom:hover img {
+    -webkit-transform: scale(1.1);
+    transform: scale(1.1);
+  }
+  .equal-height {
+    width: 100%; /* Make images responsive */
+    height: auto; /* Maintain aspect ratio */
+    height: 100%; /* Make sure the height fills the column */
+    object-fit: cover; /* Ensure the image covers the area */
+  }
 
 /* Style pour nos services */
 
@@ -316,123 +196,8 @@ export default {
   text-decoration: underline;
 }
 
-
-
-/* Style pour le header et nav bar */
-
-.logo {
-  width: 50px;
-  height: 50px;
-  border-radius: 50%;
-}
-
-  .icon {
-      width: 50px; /* Set the width of the icon */
-      height: auto; /* Maintain aspect ratio */
-      margin-bottom: 5px; /* Spacing between icon and text */
-  }
-  
-  .text-end {
-      display: flex;
-      justify-content: flex-end; /* Align items to the end */
-  }
-  
-  .container {
-      margin-top: 20px; /* Optional spacing */
-  }
-  
-  .equal-height {
-    width: 100%; /* Make images responsive */
-    height: auto; /* Maintain aspect ratio */
-      height: 100%; /* Make sure the height fills the column */
-      object-fit: cover; /* Ensure the image covers the area */
-  }
-  
-  .overflow {
-      position: relative;
-      overflow: hidden;
-  }
-  
-  .zoom img {
-      transition: all 0.2s linear;
-  }
-  
-  .zoom:hover img {
-      -webkit-transform: scale(1.1);
-      transform: scale(1.1);
-  }
-  
-  .navbar {
-    background-color: #333;
-    color: #fff;
-    padding: 15px;
-
-    align-items: center;
-    display: flex;
-    padding: 10px 20px;
-  }
-  
-  .navbar-container {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-
-    flex: 1;
-    justify-content: center;
-    padding: 10px 20px;
-  }
-  /* .navbar-logo a {
-    color: #fff;
-    text-decoration: none;
-    font-size: 24px;
-  } */
-  .navbar-links {
-    list-style: none;
-    display: flex;
-    gap: 30px;
-  }
-  
-  .navbar-links li {
-  position: relative;
-  }
-
-  .navbar-links a {
-    color: #fff;
-    text-decoration: none;
-  }
-  
-  .dropdown {
-    position: relative;
-  }
-  
-  .dropdown-menu {
-    position: absolute;
-    top: 100%;
-    left: 0;
-    background-color: #444;
-    list-style: none;
-    padding: 10px;
-    display: block;
-    min-width: 150px;
-    box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);
-  }
-  
-  .dropdown-menu li {
-    padding: 5px 0;
-  }
-  
-  .dropdown-menu a {
-    color: #fff;
-    text-decoration: none;
-  }
-  
-  .dropdown-menu a:hover {
-    color: #ccc;
-  }
-
-
-
- /* Banner Styles */
+ 
+  /* Banner Styles */
 .banner {
   background-color: #add8e6;
   padding: 15px;
@@ -501,183 +266,14 @@ export default {
 }
 
 
-
-
-
-
-
-
-  /* style pour footer */
-  .container {
-  max-width: 1200px;
-  margin: 0 auto;
-}
-
-.footer {
-  background-color: #f1f1f1;
-  padding: 20px;
-  text-align: center;
-}
-
-.container {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
-
-.newsletter {
-  margin-bottom: 20px;
-  text-align: center;
-}
-
-.footer-links {
-  display: flex;
-  width: 100%;
-  max-width: 1200px;
-  margin-bottom: 20px;
-
-  justify-content: space-between;
-  flex-wrap: wrap;
-}
-
-
-h5 {
-  margin-bottom: 10px;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-ul li {
-  margin: 5px 0;
-}
-
-a {
-  text-decoration: none;
-  color: #333;
-}
-
-a:hover {
-  text-decoration: underline;
-}
-
-.footer-bottom {
-  width: 100%;
-  border-top: 1px solid #ccc;
-  padding-top: 10px;
-  text-align: center;
-}
-
-.footer-top {
-  width: 100%;
-  border-top: 1px solid #ccc;
-  padding-top: 10px;
-  text-align: center;
-}
-
-.big-footer-bottom {
-  width: 100%;
-  border-bottom: 1px solid #ccc;
-  padding-bottom: 10px;
-  text-align: center;
-}
-
-
-.logo {
-  width: 50px; /* Adjust as needed */
-  height: 50px;
-}
-
-.small {
-  width: 60px;
-  height: 70px;
-}
-
-.legal-links {
-  display: flex;
-  justify-content: center;
-  margin-bottom: 10px;
-}
-
-.legal-links li {
-  margin: 0 15px;
-}
-
-
-.column {
-  flex: 1; /* Makes columns flexible */
-  padding: 10px; /* Adds spacing around each column */
-}
-
-.column ul {
-  list-style-type: none; /* Removes bullets */
-  padding: 0; /* Removes default padding */
-}
-
-.column img.icon {
-  display: block; /* Ensures icons are block elements */
-  margin: 0 auto; /* Centers the image */
-}
-
-@media (max-width: 768px) {
-  .logo {
-    display: block; /* Assurez-vous qu'ils s'affichent */
-  }
-}
-img {
-  max-width: 100%; /* Permet aux images de rester dans les limites de leur conteneur */
-  height: auto; /* Maintient le rapport d'aspect */
-}
-
-/* Style pour banner truspilot */
-.banner-row {
-            display: flex;
-            justify-content: space-between;
-            align-items: center;
-            background-color: #f0f0f0;
-            border-top: 1px solid #ddd;
-            border-bottom: 1px solid #ddd;
-            width: 100%;
-            max-width: 100%;
-            margin: 0 auto;
-        }
-
-        .trustpilot-column, .app-column {
-            display: flex;
-            align-items: center;
-            justify-content: center;
-            text-align: center;
-        }
-
-        .trustpilot-column img {
-            margin-right: 10px;
-        }
-
-        .app-column img {
-            margin-left: 10px;
-        }
-
-        .app-column {
-            gap: 10px;
-        }
-
-        .banner-row p {
-            margin: 0;
-            font-size: 16px;
-            color: #333;
-        }
-
-
-/* contact form */
-body {
+          /* contact form */
+          body {
             background: linear-gradient(to bottom right, #1e90ff, #00ced1);
             font-family: Arial, sans-serif;
             margin: 0;
             padding: 0;
         }
-
+        
         .contact-form {
             background-color: white;
             padding: 30px;
@@ -685,15 +281,15 @@ body {
             box-shadow: 0 4px 10px rgba(0, 0, 0, 0.1);
             max-width: 800px; /* Maximum width of the form */
             margin: 20px auto; /* Center the form with auto margins */
-        }
-
+          }
+          
         .contact-form h2 {
-            margin-top: 0;
-            color: #333;
+          margin-top: 0;
+          color: #333;
         }
 
         .contact-form label {
-            display: block;
+          display: block;
             margin-top: 15px;
             margin-bottom: 5px;
             font-weight: bold;
@@ -708,7 +304,7 @@ body {
             border: 1px solid #ccc;
             border-radius: 5px;
         }
-
+        
         .contact-form button {
             margin-top: 20px;
             padding: 10px;
@@ -729,3 +325,20 @@ body {
             margin-top: 15px;
         }
 </style>
+            
+            
+            
+            <script>
+            export default {
+              methods: {
+                subscribe() {
+                  alert('Inscription à la newsletter réussie !');
+                }
+              },
+              data() {
+                  return {
+                    showDropdown: false,
+                  };
+                },
+            }
+            </script>
